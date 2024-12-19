687b5e12e770ea54b49bbeda393af8acba4a798b
hey-oise <oise.giwa.okugbe@gmail.com>
2024-12-19 21:46:15 +0100

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory =["stick"];
+const button1 = document.querySelector("#button1")
+const button2 = documents.querySelector("#button2")
+const button3 = documents.querySelector("#button3")
+const text = documents.querySelector("#text")
+const xpText = documents.querySelector("#xpText")
+const healthText = documents.querySelector("#healthText")
+const goldText = documents.querySelector("#goldText")
+const monsterStats = documents.querySelector("#monsterStats")
+const monsterNameText = documents.querySelector("#monsterNameText")
+const monsterHealthText = documents.querySelector("#monsterHealthText")
+//inialize buttons
